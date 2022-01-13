934a2542250c316b44e2852a1a7f0deac495b08e
xugaoxiang <xugx.ai@gmail.com>
2022-01-14 00:23:41 +0800

    add

--- 3_ 视频保存成H264编码/main.py
+++ 3_ 视频保存成H264编码/main.py
@@ -1,3 +1,7 @@
+'''
+这里使用cisco开源的openh264来实现h264编码格式的视频存储，ffmpeg默认并不支持h264，需要重新编译源码，相对麻烦
+'''
+
 import cv2
 
 cap = cv2.VideoCapture('test.mp4')
@@ -5,7 +9,7 @@ cap = cv2.VideoCapture('test.mp4')
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fps = cap.get(cv2.CAP_PROP_FPS)
-fourcc = int(cv2.VideoWriter_fourcc(*'H264'))
+fourcc = int(cv2.VideoWriter_fourcc(*'avc1'))
 
 out = cv2.VideoWriter('output.mp4', fourcc, fps, (width,  height))
 
