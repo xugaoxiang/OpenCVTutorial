eb1a7b051e8340fa14fe1ac88c0ecf3f93e007e4
xugaoxiang <xugx.ai@gmail.com>
2022-02-16 15:30:30 +0800

    add opencv-15

--- 15_像素归一化/main.py
+++ 15_像素归一化/main.py
@@ -4,34 +4,35 @@ import numpy as np
 image = cv2.imread('lenna.png')
 cv2.imshow("original image", image)
 
+# 灰度图
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # 转换为浮点数类型数组
 gray = np.float32(gray)
 print(gray)
 
-# scale and shift by NORM_MINMAX
+# NORM_MINMAX，最常用的一种方法，数组的数值会被缩放到一个指定的范围，比如本例中的 0~1
 dst = np.zeros(gray.shape, dtype=np.float32)
-cv2.normalize(gray, dst=dst, alpha=0, beta=1.0, norm_type=cv2.NORM_MINMAX)
-print(dst)
+# 这里alpha=1, beta=0也是ok的
+cv2.normalize(gray, dst=dst, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
+# 显示原图时，需要将像素值 re-scale 到 0~255
 cv2.imshow("NORM_MINMAX", np.uint8(dst*255))
 
-# scale and shift by NORM_INF
+# NORM_INF，无穷范数，每个值除以最大值来进行无穷范数归一化
 dst = np.zeros(gray.shape, dtype=np.float32)
 cv2.normalize(gray, dst=dst, alpha=1.0, beta=0, norm_type=cv2.NORM_INF)
-print(dst)
+# print(dst)
 cv2.imshow("NORM_INF", np.uint8(dst*255))
 
-# scale and shift by NORM_L1
+# NORM_L1，1范数，每个值除以它们的和来进行归一化
 dst = np.zeros(gray.shape, dtype=np.float32)
 cv2.normalize(gray, dst=dst, alpha=1.0, beta=0, norm_type=cv2.NORM_L1)
 print(dst)
 cv2.imshow("NORM_L1", np.uint8(dst*10000000))
 
-# scale and shift by NORM_L2
+# NORM_L2，2范数，每个值除以该向量的模长，归一化位单位向量
 dst = np.zeros(gray.shape, dtype=np.float32)
 cv2.normalize(gray, dst=dst, alpha=1.0, beta=0, norm_type=cv2.NORM_L2)
-print(dst)
 cv2.imshow("NORM_L2", np.uint8(dst*10000))
 
 cv2.waitKey(0)
